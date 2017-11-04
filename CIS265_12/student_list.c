e459e8e77e2f37e0631747b5207a76b90414c89e
Jim Eli <31859133+JimEli@users.noreply.github.com>
2017-11-04 16:20:13 -0600

    Update student_list.c

--- CIS265_12/student_list.c
+++ CIS265_12/student_list.c
@@ -18,9 +18,14 @@
 *************************************************************************/
 #include "student_list.h"
 
-// Debug memory allocation routines.
 #ifdef _DEBUG
+// Debug memory allocation routines.
 #include "debugMalloc.h"
+
+// Macros defined after our replacement functions.
+#define malloc(size) _debugMalloc(size, __FILE__, __LINE__)
+#define free(p) _debugFree(p, __FILE__, __LINE__)
+#define exit(s) _debugExit(s)
 #endif
 
 // C/C++ Preprocessor Definitions: _CRT_SECURE_NO_WARNINGS
