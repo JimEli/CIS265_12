4fc3f8da2eb0554975198927982bd2ce88ec3293
Jim Eli <31859133+JimEli@users.noreply.github.com>
2017-11-04 09:25:16 -0600

    Update student_list.h

--- CIS265_12/student_list.h
+++ CIS265_12/student_list.h
@@ -16,6 +16,13 @@
 * Change Log:
 *   10/14/2017: Initial release. JME
 *************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <assert.h>
+#include <ctype.h>
+
 #ifndef _STUDENT_LIST_H_
 #define _STUDENT_LIST_H_
 
