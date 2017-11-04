f2f20320b5b8d01fa671ef529860423e17182196
Jim Eli <31859133+JimEli@users.noreply.github.com>
2017-11-04 10:24:27 -0600

    Update debugMalloc.h

--- CIS265_12/debugMalloc.h
+++ CIS265_12/debugMalloc.h
@@ -9,15 +9,18 @@
 #include <limits.h>
 
 #ifdef _DEBUG
-static unsigned char _deadLandFill = 0xDD;  /* fill free objects with this */
-static unsigned char _cleanLandFill = 0xCC; /* fill new objects with this */
 
+// Memory paint values.
+static unsigned char _deadLandFill = 0xDD;  // Fill free objects with this value.
+static unsigned char _cleanLandFill = 0xCC; // Fill new objects with this value.
+// Memory statistics.
 static size_t currentMemory = 0;
 static size_t peakMemory = 0;
-
+// Redirection function definitions.
 void *debugMalloc(size_t nSize, char *, int);
 void debugFree(void *pMem, char *, int);
 
+// Include the redirection code which re-defines the malloc/free calls via macros.
 #include "debugMalloc.c"
 
 #endif
